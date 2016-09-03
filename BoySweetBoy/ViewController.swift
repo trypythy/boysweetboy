0dffbf88bf7d619dfee9be8d6eabe122f20035dd
trypythy <brijeshtripathi@Brijeshs-MacBook-Pro.local>
2016-09-04 03:14:24 +0530

    Not for long though

--- BoySweetBoy/ViewController.swift
+++ BoySweetBoy/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Not too long though")
     }
 
     override func didReceiveMemoryWarning() {
